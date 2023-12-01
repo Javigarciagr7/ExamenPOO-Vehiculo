e4a2da8ef03272249fdfb425ca6b6ef9903c6ad8
Javier García <javiergarciarodriguez@alumnos.tuniversformacion.es>
2023-12-01 14:10:55 +0100

    hecho

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,21 @@ vehiculo1.arrancar();
 vehiculo1.acelerar(160);
 vehiculo1.frenar();
 
+//variable tipo object de un objeto vehiculo
+const vehiculoObject ={
+    marca: 'BMW',
+    modelo: 'M competition',
+    color: 'rojo',
+    fabricacion: 2021,
+    cilindrada: '411'
+}
+
+//almacenar datos: guardor un JSON en el LocalStorage
+localStorage.setItem("vehiculo", JSON.stringify(vehiculoObject))
+
+//recuperar datos: mostrar el object en consola del JSON que hemos guardado en localStorage
+console.log(localStorage.getItem("Vehiculo"));
+
 //Muestra un objeto furgoneta
 const furgoneta1 = new Furgoneta('Ctroen', 'C15', 'blanca', 1995, '120', 10);
 mostrarResultado(`<br/><br/>--Furgoneta 1--`);
@@ -28,10 +43,31 @@ furgoneta1.arrancar();
 furgoneta1.acelerar(350);
 furgoneta1.frenar();
 
+
+//Guardar en el localStorage la marca de la furgoneta
+localStorage.setItem("marca furgoneta", furgoneta1.marca);
+
 //Muestra un objeto furgoneta
 const todoterreno1 = new Furgoneta('Mitshubisi', 'Pajero', 'gris', 1995, '190', 4);
 mostrarResultado(`<br/><br/>--Furgoneta 1--`);
 todoterreno1.mostrardatos();
+mostrarResultado(`<br/> Tiene traccion` +todoterreno1.traccion)
 todoterreno1.arrancar();
 todoterreno1.acelerar(250);
 todoterreno1.frenar();
+
+const todoterrenoObject ={
+    marca: todoterreno1.marca,
+    modelo: todoterreno1.modelo,
+    color: todoterreno1.color,
+    fabricacion: todoterreno1.fabricacion,
+    cilindrada: todoterreno1.cilindrada,
+    traccion: todoterreno1.traccion
+}
+
+//Almacenar los datos en localStorage mediante un bucle iterativo
+for(let key in todoterrenoObject){
+    if(todoterrenoObject.hasOwnProperty(key)){
+        localStorage.setItem(key, todoterrenoObject[key]);
+    }
+}
